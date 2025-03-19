915a9c408fa350f284e73d43492c877c5b708483
Sanafar-coder <sanafar3870@gmail.com>
2025-03-19 15:08:57 +0530

    Add files via upload

---
+++ StringBuild.java
@@ -0,0 +1,13 @@
+package day3.com;
+
+public class StringBuild {
+   public static void main(String args[]) {
+	   StringBuilder sb = new StringBuilder();
+	   
+	   sb.append("hello");
+	   System.out.println(sb);
+	   sb.reverse();
+	   System.out.println(sb);
+	   
+	   }
+}
